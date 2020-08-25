b0973e4ab4f4ee57d29243f79eb37390c7eefc31
Denys Chornyi <chornijj14@gmail.com>
2020-08-26 00:11:53 +0200

    Divide element to single components

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo App</h1>
-    <input placeholder="search"/>
+const TodoHeader = () => <h1>My Todo App</h1>;
+
+const SearchPanel = () => <input placeholder="search"/>;
+
+const TodoList = () => {
+  return (
     <ul>
       <li>Learn React</li>
       <li>Be a PRO in React</li>
       <li>Start working at Netguru as Frontend developer in September</li>
     </ul>
-  </div>
-);
+  );
+}
 
-ReactDOM.render(el , document.getElementById('root'));
+const App = () => {
+return (
+    <div>
+      <TodoHeader/>
+      <SearchPanel/>
+      <TodoList/>
+    </div>
+  );
+}
+ReactDOM.render(<App/> , document.getElementById('root'));
