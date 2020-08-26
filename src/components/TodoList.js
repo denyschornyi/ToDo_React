6b534d9b43fb736da4ed7ae4e4f495e1fb8fec3d
Denys Chornyi <chornijj14@gmail.com>
2020-08-26 16:33:02 +0200

    TodoItem added

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import TodoListItem from './TodoListItem'
+
 const TodoList = () => {
     return (
       <ul>
-        <li>Learn React</li>
+        <li><TodoListItem/></li>
+        {/* <li>Learn React</li>
         <li>Be a PRO in React</li>
-        <li>Start working at Netguru as Frontend developer in September</li>
+        <li>Start working at Netguru as Frontend developer in September</li> */}
       </ul>
     );
 }
