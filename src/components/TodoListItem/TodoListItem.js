93ae0706ea88bc04cfb8e82dd7e777546aec548e
Denys Chornyi <chornijj14@gmail.com>
2020-08-28 21:37:12 +0200

    Hand over this to arrow function

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -4,6 +4,11 @@ import './TodoListItem.css';
 
 
 export default class TodoListItem extends Component {
+
+  onLabelClick = () => { 
+    console.log(` Done: ${this.props.label} `);
+  }
+
   render (){
 
     const { label, important = false } = this.props;
@@ -17,13 +22,15 @@ export default class TodoListItem extends Component {
       <span className="todo-list-item">
         <span
           className="todo-list-item-label"
-          style={style}>
+          style={style}
+          onClick={ this.onLabelClick } >
+
           {label}
         </span>
   
         <button type="button"
                 className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation" />
+                <i className="fa fa-exclamation" />
         </button>
   
         <button type="button"
